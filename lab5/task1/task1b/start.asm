dfe911639774f9640a4e1c9b17f941b5f63362e0
eric12s <ericsabag@gmail.com>
2021-11-26 03:43:06 +0200

    part of task1b

---
+++ lab5/task1/task1b/start.asm
@@ -0,0 +1,190 @@
+section .bss 
+	arr: resb 32 ;reserve location array len
+
+section .text
+	
+global _start
+global read
+global write
+global open
+global close
+global strlen
+global utoa_s
+global atou_s
+
+extern main
+_start:
+	; My code
+	mov eax, [esp] ; argc
+	lea edx , [esp+4] ; argv
+	push edx 
+	push eax
+
+	call	main
+    mov     ebx,eax
+	mov	eax,1
+	int 0x80
+
+read:
+    enter 0, 0 ; replace "push ebp mov ebp, esp"
+
+	push ebx
+	push ecx
+	push edx
+
+	mov eax,3
+	mov ebx,[ebp+8] ; fd
+	mov ecx,[ebp+12] ; buffer
+	mov edx,[ebp+16] ; size
+	int 0x80
+
+	pop edx
+	pop ecx
+	pop ebx
+	leave ;replace "mov esp, ebp pop ebp"
+	ret
+
+write:
+    enter 0, 0
+
+	push ebx
+	push ecx
+	push edx
+
+	mov eax,4
+	mov ebx,[ebp+8] ;fd
+	mov ecx,[ebp+12] ;buffer
+	mov edx,[ebp+16] ;size
+
+	int 0x80
+
+	pop edx
+	pop ecx
+	pop ebx
+	leave
+	ret
+
+open:
+    enter 0, 0
+
+	push ebx
+	push ecx
+
+	mov eax,5
+	mov ebx,[ebp+8] ; string
+	mov ecx,[ebp+12] ; flag(r, w...)
+	int 0x80
+
+	pop ecx
+	pop ebx
+	leave
+	ret
+
+close:
+    enter 0, 0
+
+	push ebx
+
+	mov eax,4
+	mov ebx,[ebp+8] ; flag
+	int 0x80
+
+	pop ebx
+	leave
+	ret
+
+strlen:
+    enter 0, 0
+	mov eax,-1
+	jmp .loop
+
+.loop:
+	add eax, 1
+	mov ebx, eax
+	add ebx, [ebp+8]
+	movzx ebx, BYTE [ebx]
+	test bl,bl
+	jne .loop
+	jmp .end_loop
+
+.end_loop:
+	pop ebx
+	leave
+	ret
+
+utoa_s:
+    enter 0, 0
+	sub esp, 8
+	pushad ; push the registers
+
+	mov ecx, arr
+
+	mov eax, [ebp+8]
+
+	mov byte [ecx+15], 0 ; end
+	mov dword [ebp-8], 0xF ;counter
+
+	cmp eax, 0
+	je exit
+
+loop:
+	mov edx, 0
+	mov ecx, 10
+	div ecx ; eax/ecx 
+	mov [ebp-4], edx ; [ebp-4] = edx - stores the reminder
+	add dword [ebp-4], 0x30  ; ans result to ascii value
+	mov edx, [ebp-4] ; edx = value
+	dec dword [ebp-8]
+	mov ecx , [ebp-8]
+	add ecx , arr ;ecx = arr[counter]
+	mov [ecx] , dl
+	cmp eax , 0
+	jne loop
+	je exit
+
+atou_s:
+    enter 0, 0
+	sub esp, 8
+	pushad ; push the registers
+
+	mov ecx, arr
+
+	mov eax, [ebp+8]
+
+	mov byte [ecx+15], 0 ; end
+	mov dword [ebp-8], 0xF ;counter
+
+	cmp eax, 0
+	je exit
+
+loop_atou:
+	mov edx, 0
+	mov ecx, 10
+	div ecx ; eax/ecx 
+	mov [ebp-4], edx ; [ebp-4] = edx - stores the reminder
+	; add dword [ebp-4], 0x30  ; ans result to ascii value
+	mov edx, [ebp-4] ; edx = value
+	dec dword [ebp-8]
+	mov ecx , [ebp-8]
+	add ecx , arr ;ecx = arr[counter]
+	mov [ecx] , dl
+	cmp eax , 0
+	jne loop_atou
+	je exit
+
+end:
+	mov esp, ebp
+	pop ebx
+	pop ebp
+	ret
+
+exit:
+	mov edx, [ebp-8]
+	add edx, arr
+	mov [ebp-4], edx ;local var = potr
+
+	popad
+	mov eax, [ebp-4] ;return value
+
+	leave
+	ret
