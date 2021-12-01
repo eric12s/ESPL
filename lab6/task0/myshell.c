a61930d9ed6bac197f9b663d05283850d89d5bc3
eric12s <ericsabag@gmail.com>
2021-12-02 01:18:18 +0200

    start lab6

---
+++ lab6/task0/myshell.c
@@ -0,0 +1,37 @@
+#include <linux/limits.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "LineParser.h"
+
+void execute(cmdLine *cmdLine) {
+    int result = execvp(cmdLine->arguments[0], cmdLine->arguments);
+
+    if (result == -1) {
+        perror("ERROR");
+    }   
+}
+
+int main(int argc, char const *argv[]){
+    char input[2048];
+    char buff[PATH_MAX];
+    int const MAX_LINE_SIZE = 2048;
+
+    while (1) {
+        getcwd(buff, PATH_MAX);
+        printf("%s> ", buff);
+        fgets(input, MAX_LINE_SIZE, stdin);
+
+
+        if (strcmp(input, "quit\n") == 0)
+            break;
+
+        cmdLine *line = parseCmdLines(input);
+        execute(line);
+        freeCmdLines(line);
+    }
+
+    printf("\n");
+    return 0;
+    return 0;
+}
